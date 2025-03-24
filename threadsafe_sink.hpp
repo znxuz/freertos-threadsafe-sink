dca1d11707859b3cece6a397624b2a124f25004f
Zijian Xu <znxuz@proton.me>
2025-03-24 14:44:30 +0100

    renames; FIXME race conditions

--- threadsafe_sink.hpp
+++ threadsafe_sink.hpp
@@ -7,7 +7,7 @@
 namespace freertos {
 enum struct CALLSITE { ISR, NON_ISR };
 
-typedef void (*csink_consume_f)(const uint8_t* buf, size_t size);
+typedef void (*tsink_consume_f)(const uint8_t* buf, size_t size);
 
 namespace detail {
 #ifndef SINK_SIZE
@@ -22,44 +22,46 @@ inline uint8_t sink[SINK_SIZE];
 inline bool consumable[SINK_SIZE];
 inline volatile size_t write_idx;
 
-inline csink_consume_f consume;
+inline tsink_consume_f consume;
 
 inline void consume_and_wait(size_t pos, size_t size) {
   auto update_for_writer = [](size_t pos, size_t size) static {
-    for (size_t i = 0; i < size; ++i) consumable[pos++] = false;
+    for (size_t i = 0; i < size; ++i) consumable[pos + i] = false;
   };
 
   consume(sink + pos, size);
-  ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+  ulTaskNotifyTake(pdFALSE, portMAX_DELAY);
   update_for_writer(pos, size);
 }
 
 inline void task_impl(void*) {
-  size_t start = 0;
+  size_t pos = 0;
   while (true) {
     xSemaphoreTake(task_semphr, portMAX_DELAY);
 
     auto end = write_idx;
-    if (start == end) continue;  // just for safety
+    if (pos == end && !consumable[pos]) continue;
 
-    auto size = (start < end ? end : SINK_SIZE) - start;
-    consume_and_wait(start, size);
-    if (start > end && end) consume_and_wait(0, end);
+    auto size = (pos < end ? end : SINK_SIZE) - pos;
+    consume_and_wait(pos, size);
+    if (pos >= end && end) consume_and_wait(0, end);
 
-    start = end;
+    pos = end;
   }
 }
 }  // namespace detail
 
 // write `len` from `ptr` buffer into the sink
-inline void csink_write(const char* ptr, size_t len) {
+inline void tsink_write(const char* ptr, size_t len) {
   xSemaphoreTake(detail::write_mtx, portMAX_DELAY);
   for (size_t i = 0; i < len; ++i) {
     while (detail::consumable[detail::write_idx]) vTaskDelay(1);
 
     detail::sink[detail::write_idx] = ptr[i];
+    taskENTER_CRITICAL();
     detail::consumable[detail::write_idx] = true;
     detail::write_idx = (detail::write_idx + 1) % detail::SINK_SIZE;
+    taskEXIT_CRITICAL();
   }
   xSemaphoreGive(detail::task_semphr);
   xSemaphoreGive(detail::write_mtx);
@@ -67,9 +69,9 @@ inline void csink_write(const char* ptr, size_t len) {
 
 // callback upon consume completion to signal the sink task
 template <CALLSITE context>
-void csink_consume_complete() {
-  static BaseType_t xHigherPriorityTaskWoken;
+inline void tsink_consume_complete() {
   if constexpr (context == CALLSITE::ISR) {
+    static BaseType_t xHigherPriorityTaskWoken;
     vTaskNotifyGiveFromISR(detail::task_hdl, &xHigherPriorityTaskWoken);
     portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
   } else {
@@ -78,10 +80,11 @@ void csink_consume_complete() {
 }
 
 // init function taking a function pointer to consume the bytes in the sink
-inline void csink_init(csink_consume_f f, uint32_t priority) {
+inline void tsink_init(tsink_consume_f f, uint32_t priority) {
   detail::consume = f;
 
-  static StaticSemaphore_t write_mtx_buffer, task_semphr_buffer;
+  static StaticSemaphore_t write_mtx_buffer;
+  static StaticSemaphore_t task_semphr_buffer;
   configASSERT(
       (detail::write_mtx = xSemaphoreCreateMutexStatic(&write_mtx_buffer)));
   configASSERT((detail::task_semphr =
@@ -91,7 +94,7 @@ inline void csink_init(csink_consume_f f, uint32_t priority) {
   static StackType_t task_stack[STACK_SIZE];
   static StaticTask_t task_buffer;
   configASSERT((detail::task_hdl = xTaskCreateStatic(
-                    detail::task_impl, "cs", STACK_SIZE, NULL, priority,
+                    detail::task_impl, "tsink", STACK_SIZE, NULL, priority,
                     task_stack, &task_buffer)) != NULL)
 }
 }  // namespace freertos
