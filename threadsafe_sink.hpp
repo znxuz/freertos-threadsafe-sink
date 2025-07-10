6ccc0059e4a257e9778e486409722fff33da8d7a
Zijian Xu <znxuz@proton.me>
2025-07-10 22:08:16 +0200

    objectify

--- threadsafe_sink.hpp
+++ threadsafe_sink.hpp
@@ -3,17 +3,16 @@
 #include <FreeRTOS.h>
 #include <semphr.h>
 
+#include <array>
 #include <atomic>
 #include <concepts>
+#include <span>
 #include <type_traits>
 
-namespace freertos::tsink {
+namespace freertos {
 enum struct CALL_FROM { ISR, NON_ISR };
 
-using consume_fn = void (*)(const uint8_t*, size_t);
-
-namespace detail {
-// TODO: support for half-word & word length
+// TODO: variable atomic length support
 template <typename T>
 concept Elem = std::same_as<T, uint8_t> || std::same_as<T, char>;
 
@@ -24,130 +23,121 @@ concept ElemContainer = requires(C t) {
   t.size();
 };
 
-#ifndef TSINK_CAPACITY
-inline constexpr size_t TSINK_CAPACITY = 2048;
-#endif
+template <size_t N>
+struct tsink {
+  struct mtx_guard {
+    explicit mtx_guard(SemaphoreHandle_t write_itx) : mtx{write_itx} {
+      configASSERT(xSemaphoreTake(mtx, portMAX_DELAY));
+    }
+    ~mtx_guard() { configASSERT(xSemaphoreGive(mtx)); }
 
-inline TaskHandle_t task_hdl;
-inline SemaphoreHandle_t write_mtx;
-
-inline uint8_t sink[TSINK_CAPACITY]{};
-inline volatile size_t read_idx = 0;
-inline std::atomic<size_t> write_idx = 0;
-
-inline volatile size_t ticket_matcher = 0;
-
-inline consume_fn consume;
-
-inline size_t size() { return write_idx - read_idx; }
-
-inline size_t space() { return TSINK_CAPACITY - size(); }
-
-inline size_t normalize(size_t idx) { return idx % TSINK_CAPACITY; }
-
-struct mtx_guard {
-  mtx_guard() { configASSERT(xSemaphoreTake(write_mtx, portMAX_DELAY)); }
-  ~mtx_guard() { configASSERT(xSemaphoreGive(write_mtx)); }
-};
-
-inline void task_impl(void*) {
-  auto consume_and_wait = [](size_t pos, size_t size) static {
-    if (!size) return;
-    consume(sink + pos, size);
-    ulTaskNotifyTake(pdFALSE, portMAX_DELAY);
+    SemaphoreHandle_t mtx = nullptr;
   };
 
-  while (true) {
-    if (size_t sz = size(); sz) {
-      auto idx = normalize(read_idx);
-      auto wrap_around = ((idx + sz) / TSINK_CAPACITY) *  // either 1 or 0
-                         ((idx + sz) % TSINK_CAPACITY);   // actual amount
-      auto immediate = sz - wrap_around;
-      consume_and_wait(idx, immediate);
-      consume_and_wait(0, wrap_around);
-      read_idx += sz;
-    } else {
+  using consume_fn_ptr = void (*)(const uint8_t*, size_t);
+
+  using size_type = size_t;
+
+  tsink(consume_fn_ptr f, uint32_t priority) : consume_fn{f} {
+    auto task_impl = [](void* arg) {
+      auto consume_and_wait = [](std::span<const uint8_t> ringbuf, size_t pos,
+                                 size_t size, consume_fn_ptr consume_fn) {
+        if (!size) return;
+        consume_fn(ringbuf.data() + pos, size);
+        ulTaskNotifyTake(pdFALSE, portMAX_DELAY);
+      };
+
+      auto* sink = reinterpret_cast<tsink<N>*>(arg);
+
+      while (true) {
+        if (size_t sz = sink->size(); sz) {
+          auto idx = sink->normalize(sink->read_idx);
+          auto wrap_around =
+              ((idx + sz) / sink->ringbuf.size()) *  // either 1 or 0
+              ((idx + sz) % sink->ringbuf.size());   // actual amount
+          auto immediate = sz - wrap_around;
+          consume_and_wait(sink->ringbuf, idx, immediate, sink->consume_fn);
+          consume_and_wait(sink->ringbuf, 0, wrap_around, sink->consume_fn);
+          sink->read_idx += sz;
+        } else {
+          vTaskDelay(pdMS_TO_TICKS(1));
+        }
+      }
+    };
+
+    static StaticSemaphore_t write_mtx_buffer;
+    configASSERT((write_mtx = xSemaphoreCreateMutexStatic(&write_mtx_buffer)));
+
+    constexpr size_t STACK_SIZE = 512;
+    static StackType_t task_stack[STACK_SIZE];
+    static StaticTask_t task_buffer;
+    configASSERT((consum_task_hdl = xTaskCreateStatic(
+                      task_impl, "tsink", STACK_SIZE, this, priority,
+                      task_stack, &task_buffer)) != NULL)
+  }
+
+  constexpr size_type size() { return write_idx - read_idx; }
+  constexpr size_type space() { return ringbuf.size() - size(); }
+  constexpr size_type normalize(size_type idx) { return idx % ringbuf.size(); }
+  void reset_ticket() { ticket_matcher = 0; }
+
+  bool write_or_fail(Elem auto elem) {
+    auto expected = write_idx.load();
+    if (expected - read_idx == ringbuf.size()) return false;
+    if (write_idx.compare_exchange_strong(expected, expected + 1)) {
+      ringbuf[normalize(expected)] = elem;
+      return true;
+    }
+    return false;
+  }
+
+  // write `len` from `ptr` buffer into the sink
+  void write_blocking(const Elem auto* ptr, size_t len) {
+    while (true) {
+      if (volatile auto _ = mtx_guard{write_mtx}; space() >= len) {
+        for (size_t i = 0; i < len; ++i) configASSERT(write_or_fail(ptr[i]));
+        return;
+      }
       vTaskDelay(pdMS_TO_TICKS(1));
     }
   }
-}
-}  // namespace detail
 
-using detail::size;
-using detail::space;
-
-inline void reset_ticket() { detail::ticket_matcher = 0; }
-
-inline bool write_or_fail(detail::Elem auto elem) {
-  using namespace detail;
-
-  auto expected = write_idx.load();
-  if (expected - read_idx == TSINK_CAPACITY) return false;
-  if (write_idx.compare_exchange_strong(expected, expected + 1)) {
-    sink[normalize(expected)] = elem;
-    return true;
+  void write_blocking(const ElemContainer auto& t) {
+    write_blocking(t.data(), t.size());
   }
-  return false;
-}
 
-// write `len` from `ptr` buffer into the sink
-inline void write_blocking(const detail::Elem auto* ptr, size_t len) {
-  using namespace detail;
-
-  while (true) {
-    if (volatile auto _ = mtx_guard{}; space() >= len) {
-      for (size_t i = 0; i < len; ++i) configASSERT(write_or_fail(ptr[i]));
-      return;
-    }
-    vTaskDelay(pdMS_TO_TICKS(1));
-  }
-}
-
-inline void write_blocking(const detail::ElemContainer auto& t) {
-  write_blocking(t.data(), t.size());
-}
-
-// performance at the mercy of the scheduler
-inline void write_ordered(const detail::Elem auto* ptr, size_t len,
-                          size_t ticket) {
-  using namespace detail;
-
-  while (true) {
-    if (ticket == ticket_matcher) {
-      while (space() < len) vTaskDelay(pdMS_TO_TICKS(1));
-      for (size_t i = 0; i < len; ++i) configASSERT(write_or_fail(ptr[i]));
-      ticket_matcher += 1;
-      return;
+  // performance at the mercy of the scheduler
+  inline void write_ordered(const Elem auto* ptr, size_t len, size_t ticket) {
+    while (true) {
+      if (ticket == ticket_matcher) {
+        while (space() < len) vTaskDelay(pdMS_TO_TICKS(1));
+        for (size_t i = 0; i < len; ++i) configASSERT(write_or_fail(ptr[i]));
+        ticket_matcher += 1;
+        return;
+      }
     }
   }
-}
-
-// callback upon consume completion to signal the sink task
-template <CALL_FROM callsite>
-void consume_complete() {
-  using namespace detail;
-  if constexpr (callsite == CALL_FROM::ISR) {
-    static BaseType_t xHigherPriorityTaskWoken;
-    vTaskNotifyGiveFromISR(task_hdl, &xHigherPriorityTaskWoken);
-    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
-  } else {
-    xTaskNotifyGive(task_hdl);
+  //
+  // callback upon consume completion to signal the sink task
+  template <CALL_FROM callsite>
+  void consume_complete() {
+    if constexpr (callsite == CALL_FROM::ISR) {
+      static BaseType_t xHigherPriorityTaskWoken;
+      vTaskNotifyGiveFromISR(consum_task_hdl, &xHigherPriorityTaskWoken);
+      portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+    } else {
+      xTaskNotifyGive(consum_task_hdl);
+    }
   }
-}
 
-// init function taking a function pointer to consume the bytes in the sink
-inline void init(consume_fn f, uint32_t priority) {
-  using namespace detail;
-  consume = f;
+ private:
+  std::array<uint8_t, N> ringbuf{};
+  volatile size_type read_idx = 0;
+  std::atomic<size_type> write_idx = 0;
+  size_type ticket_matcher = 0;
+  TaskHandle_t consum_task_hdl = nullptr;
+  SemaphoreHandle_t write_mtx = nullptr;
+  consume_fn_ptr consume_fn = nullptr;
+};
 
-  static StaticSemaphore_t write_mtx_buffer;
-  configASSERT((write_mtx = xSemaphoreCreateMutexStatic(&write_mtx_buffer)));
-
-  constexpr size_t STACK_SIZE = 512;
-  static StackType_t task_stack[STACK_SIZE];
-  static StaticTask_t task_buffer;
-  configASSERT((task_hdl = xTaskCreateStatic(task_impl, "tsink", STACK_SIZE,
-                                             NULL, priority, task_stack,
-                                             &task_buffer)) != NULL)
-}
-}  // namespace freertos::tsink
+}  // namespace freertos
